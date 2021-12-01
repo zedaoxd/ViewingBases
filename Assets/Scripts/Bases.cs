424f21ef2df74974d315d62a1717e02bcbc4e057
Bruno Lessa <brunolessa@live.com>
2021-12-01 16:38:45 -0300

    up

---
+++ Assets/Scripts/Bases.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+[System.Serializable]
+public struct Bases 
+{
+    public float X;
+    public float Y;
+
+    public Bases(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+}
---
+++ Assets/Scripts/ViewingBases.cs
@@ -0,0 +1,43 @@
+gitusing System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewingBases : MonoBehaviour
+{
+    [SerializeField] private Vector2 i;
+    [SerializeField] private Vector2 j;
+    [SerializeField] private float X;
+    [SerializeField] private float Y;
+    [SerializeField] private Text textArea;
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        GizmosUtils.DrawVectorAtOrigin(j);
+        Gizmos.color = Color.red;
+        GizmosUtils.DrawVectorAtOrigin(i);
+        Gizmos.color = Color.yellow;
+        GizmosUtils.DrawVectorAtOrigin(CalculatePoint(X, i, Y, j));
+        UpdateTextArea();
+    }
+    
+    private Vector2 CalculatePoint(float scaler1, Vector2 point1, float scaler2, Vector2 point2)
+    {
+        return scaler1 * point1 + scaler2 * point2;
+    }
+
+    private void UpdateTextArea()
+    {
+        textArea.text = $"v = ({X}, {Y})\n" +
+                        $"i = ({i.x}, {i.y})\n" +
+                        $"j = ({j.x}, {j.y})\n" +
+                        $"LD = {AreLinearDependent(i, j)}";
+    }
+
+    private bool AreLinearDependent(Vector2 a, Vector2 b)
+    {
+        var determinant = a.x * b.y - b.x * a.y;
+        return determinant == 0;
+    }
+}
